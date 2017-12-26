22000a58e3752f1f1fec3f8e6b4c83bfb8d4854c
GSCHOF11 <gregs@indellient.com>
2017-12-26 14:51:00 -0500

    Added exists method

--- libraries/jenkins_cli.rb
+++ libraries/jenkins_cli.rb
@@ -7,36 +7,48 @@ class JenkinsCLI < Inspec.resource(1)
       Check jenkins configuration using the jenkins CLI jar.
     '
    
-    example '
-      config = {}
-      config['cmd']
+    example "
       describe jenkins_cli('dummy_service_6') do
         its('port') { should eq('6382') }
         its('slave-priority') { should eq('69') }
       end
-    '
+    "
    
     def initialize(options)
       @java_home = options['java_home'] || '/usr/bin/java'
-      @jar_path = options['jar_path'] || '/tmp/kitchen/cache/jenkins-cli.jar'
+      jar_path = options['jar_path'] || '/tmp/kitchen/cache/jenkins-cli.jar'
+      @jar = inspec.file(tmp)
       @source = options['source'] || 'localhost' 
       @port = options['port'] || 8080
       @username = options['username']
       @password = options['password']
-      @cli = "#{java_home} #{jar_path} -s #{source}:#{port}" 
+      @cli = "#{@java_home} #{@jar_path} -s #{@source}:#{@port}"
+      @credentials = "--username #{@username} --password #{@password}" if @username
+      return skip_resource "Can't find cli \"#{@jar_path}\"" if !@jar.file?
     end
    
     def plugins
-      @params = {}
-      cmd = Mixlib::ShellOut.new(@cli)
-      begin
-        cmd.run_command
-        @params['plugins'] = cmd.stdout
-      rescue Exception
-        return skip_resource "#{@file}: #{$!}"
-      end
+      exec('list-plugins')
     end
    
+    def exec(command)
+      @params = {}
+      begin
+        cmd = "#{@cli} #{command}"
+        cmd = "#{tmp} #{@credentials}" if @credentials
+        res = inspec.command(cmd)
+        @params['stdout'] = res.stdout
+        @params['stderr'] = res.stderr
+        @params['exit_status'] = res.exit_status
+      rescue Exception
+        return skip_resource "Command #{cmd} failed in error: #{$!}"
+      end
+    end
+
+    def exists?
+      @jar.file?
+    end  
+
     def method_missing(name)
       @params[name.to_s]
     end
