71a63bced1d33f96c646cd04da7e287b0151863b
gscho <greg.c.schofield@gmail.com>
2018-10-26 16:02:10 -0400

    Code to download the jenkins-cli
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- libraries/jenkins_cli.rb
+++ libraries/jenkins_cli.rb
@@ -1,5 +1,7 @@
 require 'mixlib/shellout'
 require 'english'
+require 'net/http'
+require 'uri'
 
 class JenkinsCLI < Inspec.resource(1)
   name 'jenkins_cli'
@@ -9,21 +11,39 @@ class JenkinsCLI < Inspec.resource(1)
   '
 
   example '
-      describe jenkins_cli do
-        it { should exist }
-      end
+    describe jenkins_cli do
+      it { should exist }
+    end
   '
 
   def initialize(options)
     @java_home = options[:java_home] || '/usr/bin/java'
-    jar_path = options[:jar_path] || '/tmp/kitchen/cache/jenkins-cli.jar'
-    @jar = inspec.file(jar_path)
-    return skip_resource "Can't find cli \"#{@jar_path}\"" unless @jar.file?
+    @jar_path = options[:jar_path] || '/tmp/kitchen/cache/jenkins-cli.jar'
+    @jar = inspec.file(@jar_path)
+    do_download_cli(options) unless @jar.file?
     @source = options[:source] || 'http://localhost'
     @port = options[:port] || 8080
     @credentials = "--username #{options[:username]}" if options[:username]
     @credentials += " --password #{options[:password]}" if options[:password]
-    @cli = "#{@java_home} -jar #{jar_path} -s #{@source}:#{@port}"
+    @cli = "#{@java_home} -jar #{@jar_path} -s #{@source}:#{@port}"
+  end
+
+  def download_cli(opts)
+    uri = URI.parse("http://#{@source}:#{@port}/jnlpJars/jenkins-cli.jar")
+    request = Net::HTTP::Get.new(uri)
+    request.basic_auth(opts[:username], opts[:password])
+    f = open(File.dirname(@jar_path))
+    begin
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request) do |resp|
+          resp.read_body do |segment|
+              f.write(segment)
+          end
+        end
+      end
+    ensure
+      f.close
+    end
   end
 
   def plugins
